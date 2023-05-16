40207d0296609415adf3a82186e6cc31e62810ce
MiimeSox <kontakt@malteschumacher.de>
2023-05-16 19:30:03 +0200

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Malte Schumacher on 16.05.23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
